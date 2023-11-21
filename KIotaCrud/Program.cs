f78d762ae8b812da8c89e7b7a97c2c3a61465c35
inprak02 <inprak02@USCHIZWIMO5324>
2023-11-21 15:30:10 +0530

    Add project files.

---
+++ KIotaCrud/Program.cs
@@ -0,0 +1,31 @@
+﻿using KiotaCrud.Client.Models;
+using System;
+using System.Threading.Tasks;
+using KiotaCrud.Client.Models;
+using KiotaCrud.Client;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using System.Net.Cache;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var authProvider = new AnonymousAuthenticationProvider();
+        // Create request adapter using the HttpClient-based implementation
+        var adapter = new HttpClientRequestAdapter(authProvider);
+        var client = new CrudClient(adapter); // Replace with the actual generated client class
+
+        // Example: Get contacts
+        var contacts = await client.Contact.GetAsync();
+        foreach (var contact in contacts)
+        {
+            Console.WriteLine($"Contact ID: {contact.Id}, Name: {contact.FirstName} {contact.LastName}, Age: {contact.Age}");
+        }
+     
+
+
+
+        // 
+    }
+}
